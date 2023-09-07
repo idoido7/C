8001c6f42fb9066887798db9417a559d3265c491
idoido7 <118129996+idoido7@users.noreply.github.com>
2023-09-07 12:43:39 +0300

    Update shiur1.cpp

--- shiur1.cpp
+++ shiur1.cpp
@@ -1,1 +1,44 @@
+//1
+#include <stdio.h>
 
+int main()
+{
+    char letter1, letter2, letter3;
+
+    printf("Enter the first letter: ");
+    scanf("%s", &letter1);
+
+    printf("Enter the second letter: ");
+    scanf(" %s", &letter2);
+
+    printf("Enter the third letter: ");
+    scanf(" %s", &letter3);
+    
+    printf("%c %c %c", letter3, letter2, letter1);
+    return 0;
+}
+
+
+//2
+#include <stdio.h>
+
+int main() {
+    int length, width;
+
+    printf("Enter the length of the rectangle: ");
+    scanf("%d", &length);
+
+    printf("Enter the width of the rectangle: ");
+    scanf("%d", &width);
+
+    int perimeter = 2 * (length + width);
+    int area = length * width;
+
+    printf("Perimeter: %d\n", perimeter);
+    printf("Area: %d\n", area);
+
+    return 0;
+}
+
+
+//3
